7831c26379886cd5f4d0c0163086da3e21c68bb7
JGSilverio <89946272+JGSilverio@users.noreply.github.com>
2022-11-08 22:45:13 -0500

    Updated app flask
    imported aws, imported s3upload, implemented file form

--- NETC Flask/app.py
+++ NETC Flask/app.py
@@ -1,19 +1,55 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+from BackEnd.Python.s3upload import upload_to_aws
+
 
 app = Flask(__name__,template_folder='Front End',static_folder='static')
 
-@app.route('/')
+#File key for upload form
+app.config['SECRET_KEY'] = 'NETCFILEKEY'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+#class functions
+
+#Upload file form to upload files to flask
+class UploadFileForm(FlaskForm):
+    file = FileField("file", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+#routes to html pages
+
+#initial page
+@app.route('/', methods=['GET',"POST"])
 def index():
     return render_template('index.html')
 
+#Home Page
 @app.route('/home')
 def home():
     return render_template('index.html')
 
-@app.route('/PDF-upload')
+#PDF Upload page, form upload to AWS
+@app.route('/PDF-upload', methods=['GET',"POST"])
 def PDFupload():
-    return render_template('PDF-upload.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        uploaded = upload_to_aws(__file__, 'netc-filestorage', 'File-Storage/' + secure_filename(file.filename)) #Upload to AWS
+        return "File has been uploaded."
+    return render_template('PDF-upload.html',form=form)
 
-@app.route('/video-upload')
+#Video Upload page, form upload to AWS
+@app.route('/video-upload', methods=['GET',"POST"])
 def videoupload():
-    return render_template('video-upload.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        uploaded = upload_to_aws(__file__, 'netc-filestorage', 'Video-File-Storage/' + secure_filename(file.filename)) #Upload to AWS
+        return "File has been uploaded."
+    return render_template('video-upload.html',form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
