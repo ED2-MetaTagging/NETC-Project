e53fab95cdafe86e423f381e4e8c00cb5afe671f
JGSilverio <89946272+JGSilverio@users.noreply.github.com>
2022-11-14 19:30:55 -0500

    Update New_Image-Text.py

--- NETC Flask/BackEnd/Python/New_Image-Text.py
+++ NETC Flask/BackEnd/Python/New_Image-Text.py
@@ -3,40 +3,44 @@
 from PIL import Image
 import pytesseract as pt
 import os
+from BackEnd.Python.s3upload import upload_to_aws
+
+#Bucket name for AWS
+BUCKET_NAME = 'netc-filestorage'
 	
-def main():
-	# path for the folder for getting the raw images
-	path ="C:\\Users\\ryans\\Documents\\School Work\\ED1\\PDF Image Extraction\\Images"
-	
-	# link to the file in which output needs to be kept
-	fullTempPath ="C:\\Users\\ryans\\Documents\\School Work\\ED1\\PDF Image Extraction\\outputFile.txt"
+def TesseractMain(ImagePath,imageName,fileName):
+
+
+	fullTempPath = os.path.dirname(__file__) + "/" + "outputFile.txt"
 
 	# iterating the images inside the folder
-	for imageName in os.listdir(path):
-		inputPath = os.path.join(path, imageName)
-		img = Image.open(inputPath)
+	inputPath = os.path.join(ImagePath, imageName)
+	img = Image.open(inputPath)
 
-		# applying ocr using pytesseract for python
-		pt.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-		text = pt.image_to_string(img, lang ="eng")
+	# applying ocr using pytesseract for python
+	#pt.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+	text = pt.image_to_string(img, lang ="eng")
 
-		# saving the text for appending it to the output.txt file
-		# a + parameter used for creating the file if not present
-		# and if present then append the text content
-		file1 = open(fullTempPath, "a+")
+	# saving the text for appending it to the output.txt file
+	# a + parameter used for creating the file if not present
+	# and if present then append the text content
+	file1 = open(fullTempPath, "a+")
 
-		# providing the name of the image
-		file1.write(imageName+"\n")
+	# providing the name of the image
+	file1.write(imageName+"\n")
 
-		# providing the content in the image
-		file1.write(text+"\n")
-		file1.close()
+	# providing the content in the image
+	file1.write(text+"\n")
+	file1.close()
 
 	# for printing the output file
 	file2 = open(fullTempPath, 'r')
-	print(file2.read())
-	file2.close()		
+	#print(file2.read())
+	file2.close()
+
+	uploaded = upload_to_aws(fullTempPath,BUCKET_NAME,'Image-Text-Storage/' + fileName + "/Output.txt")
+
+	os.remove(fullTempPath)		
 
 
-if __name__ == '__main__':
-	main()
+
